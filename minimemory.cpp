6ff10296bbe25bf1da13d5e7b84e6379c7a3b33c
Jock Murphy <jock@jockmurphy.com>
2018-04-27 13:50:21 -0700

    restored lost commits

--- minimemory.cpp
+++ minimemory.cpp
@@ -10,100 +10,99 @@ static void *expandPool(MiniMemoryPool *pool, uint16_t sz)
 	uint16_t ret = pool->memTop;
 	pool->memTop += sz;
 
-	char *p = (char *)&pool->memoryPool[ret];
+	char *p = (char *) &pool->memoryPool[ret];
 	return (void *) p;
 }
 
-
 void mmpReset(MiniMemoryPool *pool)
 {
-	pool->memTop = 0;
+	mmpInit(pool, pool->memoryPool, pool->poolSize);
 }
 
-
 ///////////////////////////////////////////////////////////////////////
-// All code from below this comment is from:
+// All code from below this comment based on:
 //     https://www.ibm.com/developerworks/jp/linux/library/l-memory/
+// and may have been modified
 ///////////////////////////////////////////////////////////////////////
 
-
-
-void mmpInit(MiniMemoryPool *pool, uint8_t *memPool, uint16_t poolSize) {
+void mmpInit(MiniMemoryPool *pool, uint8_t *memPool, uint16_t poolSize)
+{
 	pool->memoryPool = memPool;
 	pool->poolSize = poolSize;
 	pool->memTop = 0;
-	/* grab the last valid address from the OS*/
-	pool->last_valid_address = expandPool(pool, 0);
 
-	/* we don't have any memory to manage yet, so
-	*just set the beginning to be last_valid_address */
-	pool->managed_memory_start = pool->last_valid_address;
+	// get the last valid address from the OS
+	pool->lastValidAddress = expandPool(pool, 0);
 
-	/* Okay, we're initialized and ready to go */
+	// we don't have any memory to manage yet, so
+	// just set the beginning to be last_valid_address
+	pool->memoryStart = pool->lastValidAddress;
+
+	// Okay, we're initialized and ready to go
 	pool->initialized = true;
+
+	memset(pool->memoryPool, 0, pool->poolSize);
 }
 
 // TODO JEM: Condense this into a single 16 bit word with the high bit
 // being a flag
-struct mem_control_block {
-  bool is_available;
-  uint16_t size;
-};
+typedef struct _memoryControlBlock
+{
+		bool available;
+		uint16_t size;
+} MemoryControlBlock;
 
 void mmpFree(MiniMemoryPool *pool, void *firstbyte)
 {
-  struct mem_control_block *mcb;
+	MemoryControlBlock *mcb;
 
-  mcb = (struct mem_control_block *) (firstbyte - sizeof(struct mem_control_block));
-  mcb->is_available = true;
+	mcb = (struct memoryControlBlock *) ((uint8_t *) firstbyte
+			- sizeof(struct memoryControlBlock));
+	mcb->available = true;
 
-  return;
+	return;
 }
 
-void *mmpMalloc(MiniMemoryPool *pool, uint16_t numbytes) {
+void *mmpMalloc(MiniMemoryPool *pool, uint16_t numbytes)
+{
 	// Holds where we are looking in memory
-	void *current_location;
+	void *currentLocation;
 
 	// This is the same as current_location, but cast to a * memory_control_block
-	struct mem_control_block *current_location_mcb;
+	MemoryControlBlock *currentLocationMcb;
 
 	// This is the memory location we will return.
 	//It will * be set to 0 until we find something suitable
-	void *memory_location;
-
-	// Initialize if we haven't already done so
-//	if (!pool->initialized) {
-//		miniMemoryInit(pool);
-//	}
+	void *memoryLocation;
 
 	// The memory we search for has to include the memory
 	// control block, but the user of malloc doesn't need
 	// to know this, so we'll just add it in for them.
-	numbytes = numbytes + sizeof(struct mem_control_block);
+	numbytes = numbytes + sizeof(struct memoryControlBlock);
 
 	// Set memory_location to 0 until we find a suitable * location
-	memory_location = 0;
+	memoryLocation = 0;
 
 	// Begin searching at the start of managed memory
-	current_location = pool->managed_memory_start;
+	currentLocation = pool->memoryStart;
 
 	// Keep going until we have searched all allocated space
-	while (current_location != pool->last_valid_address) {
+	while(currentLocation != pool->lastValidAddress) {
 		// current_location and current_location_mcb point
 		// to the same address.  However, current_location_mcb
 		// is of the correct type so we can use it as a struct.
 		// current_location is a void pointer so we can use it
 		// to calculate addresses.
-		current_location_mcb = (struct mem_control_block *)current_location;
+		currentLocationMcb = (struct memoryControlBlock *) currentLocation;
 
-		if (current_location_mcb->is_available) {
-			if (current_location_mcb->size >= numbytes) {
+		if(currentLocationMcb->available) {
+			if(currentLocationMcb->size >= numbytes) {
 				// Woohoo!  We've found an open, * appropriately-size location.
 				// It is no longer available
-				current_location_mcb->is_available = false;
+				currentLocationMcb->available = false;
 
 				// We own it
-				memory_location = current_location;
+				memoryLocation = currentLocation;
 
 				// Leave the loop
 				break;
@@ -112,33 +111,97 @@ void *mmpMalloc(MiniMemoryPool *pool, uint16_t numbytes) {
 
 		// If we made it here, it's because the Current memory
 		// block not suitable, move to the next one
-		current_location = current_location + current_location_mcb->size;
+		currentLocation = ((uint8_t *) currentLocation)
+				+ currentLocationMcb->size;
 	}
 
 	// If we still don't have a valid location, we'll
 	// have to ask the operating system for more memory
-	if (!memory_location) {
+	if(!memoryLocation) {
 		// Move the program break numbytes further
 		expandPool(pool, numbytes);
 
 		// The new memory will be where the last valid * address left off
-		memory_location = pool->last_valid_address;
+		memoryLocation = pool->lastValidAddress;
 
 		// We'll move the last valid address forward * numbytes
-		pool->last_valid_address = pool->last_valid_address + numbytes;
+		pool->lastValidAddress = ((uint8_t *) pool->lastValidAddress)
+				+ numbytes;
 
 		// We need to initialize the mem_control_block
-		current_location_mcb = (mem_control_block *)memory_location;
-		current_location_mcb->is_available = false;
-		current_location_mcb->size = numbytes;
+		currentLocationMcb = (MemoryControlBlock *) memoryLocation;
+		currentLocationMcb->available = false;
+		currentLocationMcb->size = numbytes;
 	}
 
 	// Now, no matter what (well, except for error conditions),
 	// memory_location has the address of the memory, including
-	// the mem_control_block */
+	// the mem_control_block
 	// Move the pointer past the mem_control_block
-	memory_location = memory_location + sizeof(struct mem_control_block);
+	memoryLocation = ((uint8_t *) memoryLocation)
+			+ sizeof(struct memoryControlBlock);
 
 	// Return the pointer
-	return memory_location;
+	return memoryLocation;
+}
+
+///////////////////////////////////////////////////////////////////////
+// End of code based on:
+//     https://www.ibm.com/developerworks/jp/linux/library/l-memory/
+///////////////////////////////////////////////////////////////////////
+
+void *mmpRealloc(MiniMemoryPool *pool, void *ptr, uint16_t numbytes)
+{
+	// If the pointer is null, we are just a malloc
+	if(!ptr)
+	{
+		void *ret = mmpMalloc(pool, numbytes);
+		return ret;
+	}
+
+	// if the size is zero, we are just a free
+	if(numbytes == 0) {
+		mmpFree(pool, ptr);
+		return NULL;
+	}
+
+
+	MemoryControlBlock *mcb;
+
+	mcb = (struct memoryControlBlock *) ((uint8_t *) ptr
+			- sizeof(struct memoryControlBlock));
+
+	// if they are requesting a size smaller than their current size,
+	// then leave.
+	// TODO check if the delta is above a certain threshold and recover space
+	if(numbytes <= mcb->size) {
+		return ptr;
+	}
+
+	// With all that out of the way, see if we can expand the pool in place
+	// or if we have to allocate and copy
+
+	uint16_t oldLen = mcb->size;
+	uint16_t allocDelta = numbytes = oldLen;
+	uint8_t *allocTop = ((uint8_t *) ptr) + (oldLen - 1);
+
+	bool canExpand = allocTop == pool->lastValidAddress;
+
+	if(canExpand) {
+		void *tPtr = expandPool(pool, allocDelta);
+		if(tPtr == NULL) {
+			return NULL;
+		}
+
+		pool->lastValidAddress = expandPool(pool, 0);
+
+		return ptr;
+	} else {
+		void *ret = mmpMalloc(pool, numbytes);
+		if(ret) {
+			memcpy(ret, ptr, oldLen);
+			mmpFree(pool, ptr);
+		}
+		return ret;
+	}
 }
--- minimemory.h
+++ minimemory.h
@@ -15,14 +15,15 @@ typedef struct {
 		uint16_t  poolSize;
 		uint16_t  memTop;
 
-		void     *managed_memory_start;
-		void     *last_valid_address;
+		void     *memoryStart;
+		void     *lastValidAddress;
 } MiniMemoryPool;
 
 void mmpReset(MiniMemoryPool *pool);
-void miniMemoryInit(MiniMemoryPool *pool, void *memPool, uint16_t poolSize);
+void mmpInit(MiniMemoryPool *pool, void *memPool, uint16_t poolSize);
 void mmpFree(MiniMemoryPool *pool, void *firstbyte);
 void *mmpMalloc(MiniMemoryPool *pool, uint16_t numbytes);
+void *mmpRealloc(MiniMemoryPool *pool, void *prr, uint16_t numbytes);
 
 
 
